42938228c16751ed2989aeb47df409e762e684cc
MikeS <mgsuico.mentorspire@gmail.com>
2024-11-20 10:07:46 +0800

    updates: center navigation

--- App.js
+++ App.js
@@ -1,32 +1,8 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./components/screens/HomeScreen";
-import SecondScreen from "./components/screens/SecondScreen";
-
-const Stack = createStackNavigator();
+import TestNavigation from "./components/navigation/testNavigation";
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="SecondScreen"
-          component={SecondScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <TestNavigation />;
 };
 
 export default App;
--- components/navigation/testNavigation.js
+++ components/navigation/testNavigation.js
@@ -1,20 +1,32 @@
-// import React from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createStackNavigator } from "@react-navigation/stack";
-// import HomeScreen from "../screens/HomeScreen";
-// import SecondScreen from "../screens/SecondScreen";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../screens/HomeScreen";
+import SecondScreen from "../screens/SecondScreen";
 
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
-// const testNavigation = () => {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator initialRouteName="SecondScreen">
-//         <Stack.Screen name="Home" component={HomeScreen} />
-//         <Stack.Screen name="SecondScreen" component={SecondScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// };
+const testNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="SecondScreen"
+          component={SecondScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-// export default testNavigation;
+export default testNavigation;
